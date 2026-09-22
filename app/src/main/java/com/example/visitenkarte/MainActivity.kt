b29eddceab09282b2a7745669380160efb6e4264
Nils <nils.rekus@gmx.de>
2026-09-22 12:58:33 +0200

    feat(view) implement expandable FAB Menu

--- app/src/main/java/com/example/visitenkarte/MainActivity.kt
+++ app/src/main/java/com/example/visitenkarte/MainActivity.kt
@@ -5,33 +5,50 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.animation.expandVertically
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.animation.shrinkVertically
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.MailOutline
 import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.LargeFloatingActionButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -81,24 +98,14 @@ class MainActivity : ComponentActivity() {
 
             VisitenkarteTheme {
                 Scaffold(
-                    floatingActionButton = {MyFAB(Modifier.fillMaxSize())},
-                    floatingActionButtonPosition = FabPosition.EndOverlay
+                    floatingActionButton = {
+                        MyFAB()
+                                           },
+                    floatingActionButtonPosition = FabPosition.End
                 )
                 { it: PaddingValues ->
                     ListScreen(it,booksList)
                 }
-
-                //BookCoverCard(modifier = Modifier)
-
-//                    ScreenContent(
-//                        name = "John Doe",
-//                        role = "Data Scientist and Computer Vision Specialist",
-//                        "+49165409633",
-//                        smHandle = "@SmartDev",
-//                        email = "john.doe@gmail.com",
-//                        modifier = Modifier
-//                            .background(Color.Cyan)
-//                    )
             }
         }
     }
@@ -106,17 +113,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyFAB(modifier: Modifier = Modifier) {
-    var visible =  remember { mutableStateOf(true)}
-    val density = LocalDensity.current
-    LargeFloatingActionButton(
-        onClick = {
-            AnimatedVisibility(visible=visible,
-                enter = slideInVertically { -30 },
-                exit = slideOutVertically { 30 }) { }
-        },
-        shape = CircleShape,
-    ) { Icon(Icons.Filled.Add, "Large FAB") }
+    var extended by remember { mutableStateOf(false)}
+    val transition = updateTransition(targetState = extended, label = "FAB transition")
+
+    val rotation = transition.animateFloat(
+        label = "rotation",
+        targetValueByState = {if (it) 45f else 0f}
+    )
+    val items = listOf(
+        MiniFabItems(Icons.Filled.AccountCircle,"Account"),
+        MiniFabItems(Icons.Filled.Edit,"Add Book"),
+        MiniFabItems(Icons.Filled.Done,"Finished Books")
+        )
+
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.End,
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        AnimatedVisibility(
+            visible = extended,
+            enter = fadeIn() + slideInVertically(initialOffsetY = { it }) + expandVertically(),
+            exit = fadeOut() + slideOutVertically(targetOffsetY = { it }) + shrinkVertically()
+        ) {
+            Column (horizontalAlignment = Alignment.End,
+                verticalArrangement = Arrangement.spacedBy(8.dp)){
+                items.forEach { item ->
+                    MiniFABs(item.icon, item.title)
+                }
+            }
+        }
+        LargeFloatingActionButton(
+            onClick = { extended = !extended },
+            shape = CircleShape,
+        ) {
+            Icon(
+                Icons.Filled.Add,
+                "Large FAB",
+                modifier = Modifier.rotate(rotation.value)
+            )
+        }
+    }
 }
+
+@Composable
+fun MiniFABs(icon:ImageVector, title:String) {
+    Row(verticalAlignment = Alignment.CenterVertically, horizontalArrangement = Arrangement.End)
+    {
+        Spacer(Modifier.weight(1f))
+     Box(modifier = Modifier
+         .border(2.dp,
+             Color(0xffffefe5),
+             RoundedCornerShape(8.dp)
+         )
+         .padding(4.dp)){
+            Text(title)
+        }
+        FloatingActionButton(onClick = {}, modifier = Modifier.padding(3.dp)) {
+            Icon(icon, contentDescription = null)
+        }
+    }
+
+
+}
+
+data class MiniFabItems(val icon: ImageVector, val title:String)
 
 @Composable
 fun ScreenContent(name: String, role: String, phoneNumber: String, smHandle: String, email: String, modifier:Modifier) {
